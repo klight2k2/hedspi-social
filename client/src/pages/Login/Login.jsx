1df9a8dbc08206f991222b19baa95551f087a0f5
klight <phamminhquang2k2@gmail.com>
2023-12-21 10:58:10 +0700

    fix login

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -1,23 +1,24 @@
 import React, { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import { signInWithEmailAndPassword } from "firebase/auth";
-import {Button} from 'antd'
+import {Button,Form,Input,message} from 'antd'
 import { auth } from "../../firebase";
 
 import "./login.scss"
 const Login = () => {
   const [err, setErr] = useState(false);
+  const [form]=Form.useForm()
   const navigate = useNavigate();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const email = e.target[0].value;
-    const password = e.target[1].value;
-
+  const handleSubmit = async (values) => {
+    const {email,password}={...values}
     try {
       await signInWithEmailAndPassword(auth, email, password);
+      // message
       navigate("/home")
     } catch (err) {
+      message.open({type:"error",content:err.message})
+      console.log("login",err)
       setErr(true);
     }
   };
@@ -25,13 +26,33 @@ const Login = () => {
     <div className="formContainer">
       <div className="formWrapper">
         <img className="login-logo" src="/icon/hedspi.png" alt="" />
-        <form onSubmit={handleSubmit}>
-          <input type="email" placeholder="メールアドレス" />
-          <input type="password" placeholder="パスワード" />
-          <Button htmlType="submit" type="primary" size="large">ログイン</Button>
-          {err && <span>Something went wrong</span>}
-        </form>
+       
+        <Form
+          form={form}
+          autoComplete="off"
+          onFinish={handleSubmit}
+          //   onFinishFailed={onFinishFailed}
+        >
+          <Form.Item
+            name="email"
+            rules={[{ required: true, message: 'このフィールドは必須です。' }]}
+          >
+            <Input placeholder="メール" />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[{ required: true, message: 'このフィールドは必須です。' }]}
+          >
+            <Input placeholder="パスワード" />
+          </Form.Item>
+    
+         <Button htmlType="submit" type="primary" size="large">ログイン</Button>
+
+          
+         
         <p>アカウントをお持ちでないですか？ <Link to="/register">登録する</Link></p>
+        </Form>
+       
       </div>
     </div>
   );
--- client/src/pages/Register/Register.jsx
+++ client/src/pages/Register/Register.jsx
@@ -5,101 +5,139 @@ import { auth, db, storage } from '../../firebase';
 import { ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
 import { doc, setDoc } from 'firebase/firestore';
 import { useNavigate, Link } from 'react-router-dom';
-import { Button } from 'antd';
-import {UploadOutlined} from '@ant-design/icons'
+import { UploadOutlined } from '@ant-design/icons';
+import { Card, Avatar, Space, Button, Modal, Form, Input, message } from 'antd';
+
 const Register = () => {
-    const [err, setErr] = useState(false);
-    const [loading, setLoading] = useState(false);
-    const [previewUrl, setPreviewUrl] = useState();
-    const navigate = useNavigate();
+  const [err, setErr] = useState(false);
+  const [form] = Form.useForm();
+  const [selectedFile, setSelectedFile] = useState(null);
 
-    const handleSubmit = async (e) => {
-        setLoading(true);
-        e.preventDefault();
-        const displayName = e.target[0].value;
-        const email = e.target[1].value;
-        const password = e.target[2].value;
-        const file = e.target[3].files[0];
+  const [loading, setLoading] = useState(false);
+  const [previewUrl, setPreviewUrl] = useState();
+  const navigate = useNavigate();
 
-        try {
-            //Create user
-            const res = await createUserWithEmailAndPassword(auth, email, password);
+  const handleSubmit = async values => {
+    setLoading(true);
+    const { displayName, email, password } = { ...values };
+    const file = selectedFile;
 
-            //Create a unique image name
-            const date = new Date().getTime();
-            const storageRef = ref(storage, `${displayName + date}`);
+    try {
+      //Create user
+      const res = await createUserWithEmailAndPassword(auth, email, password);
 
-            await uploadBytesResumable(storageRef, file).then(() => {
-                getDownloadURL(storageRef).then(async (downloadURL) => {
-                    try {
-                        //Update profile
-                        await updateProfile(res.user, {
-                            displayName,
-                            photoURL: downloadURL,
-                        });
-                        //create user on firestore
-                        await setDoc(doc(db, 'users', res.user.uid), {
-                            uid: res.user.uid,
-                            displayName,
-                            email,
-                            photoURL: downloadURL,
-                        }).catch((err) => console.log(err));
+      //Create a unique image name
+      const date = new Date().getTime();
+      const storageRef = ref(storage, `${displayName + date}`);
 
-                        //create empty user chats on firestore
-                        await setDoc(doc(db, 'userChats', res.user.uid), {});
-                        navigate('/');
-                    } catch (err) {
-                        console.log(err);
-                        setErr(true);
-                        setLoading(false);
-                    }
-                });
+      await uploadBytesResumable(storageRef, file).then(() => {
+        getDownloadURL(storageRef).then(async downloadURL => {
+          try {
+            //Update profile
+            await updateProfile(res.user, {
+              displayName,
+              photoURL: downloadURL,
             });
-        } catch (err) {
+            //create user on firestore
+            await setDoc(doc(db, 'users', res.user.uid), {
+              uid: res.user.uid,
+              displayName,
+              email,
+              photoURL: downloadURL,
+            }).catch(err => console.log(err));
+
+            //create empty user chats on firestore
+            await setDoc(doc(db, 'userChats', res.user.uid), {});
+        message.open({type:"success",content:"アカウントの登録が成功しました。"})
+            
+            navigate('/');
+          } catch (err) {
+            console.log(err);
             setErr(true);
             setLoading(false);
-        }
-    };
-    const handleFileChange = (event) => {
-        const file = event.target.files[0];
+          }
+        });
+      });
+    } catch (err) {
+        message.open({type:"error",content:"何かエラーが発生しました。もう一度お試しください。"})
+      setErr(true);
+      setLoading(false);
+    }
+  };
+  const handleFileChange = event => {
+    const file = event.target.files[0];
+    setSelectedFile(file);
 
-        if (file) {
-            const reader = new FileReader();
+    if (file) {
+      const reader = new FileReader();
 
-            reader.onloadend = () => {
-                setPreviewUrl(reader.result);
-            };
+      reader.onloadend = () => {
+        setPreviewUrl(reader.result);
+      };
 
-            reader.readAsDataURL(file);
-        }
-    };
-    return (
-        <div className='formContainer'>
-            <div className='formWrapper'>
-                <img className='login-logo' src='/icon/hedspi.png' alt='' />
-                <form onSubmit={handleSubmit}>
-                    <input required type='text' placeholder='display name' />
-                    <input required type='email' placeholder='email' />
-                    <input required type='password' placeholder='password' />
-                    <input required style={{ display: 'none' }} type='file' id='file' onChange={handleFileChange} />
-                    <label htmlFor='file' className='file-upload'>
-                        {previewUrl && <img className='formContainer-avt' src={previewUrl} alt='' />}
-                        <div className='upload-btn'>
-                            <UploadOutlined style={{ fontSize: '20px', cursor: 'pointer' }} /> Upload avatar
-                        </div>
-                    </label>
-                    <Button htmlType='submit' size='large' type='primary' disabled={loading}>
-                        Sign up
-                    </Button>
-                    {loading && 'Uploading and compressing the image please wait...'}
-                    {err && <span>Something went wrong</span>}
-                </form>
-                <p>
-                    You do have an account? <Link to='/login'>Login</Link>
-                </p>
+      reader.readAsDataURL(file);
+    }
+  };
+  return (
+    <div className="formContainer">
+      <div className="formWrapper">
+        <img className="login-logo" src="/icon/hedspi.png" alt="" />
+        <Form
+          form={form}
+          autoComplete="off"
+          onFinish={handleSubmit}
+          //   onFinishFailed={onFinishFailed}
+        >
+          <Form.Item
+            name="displayName"
+            rules={[{ required: true, message: 'このフィールドは必須です。' }]}
+          >
+            <Input placeholder="表示名" />
+          </Form.Item>
+          <Form.Item
+            name="email"
+            rules={[{ required: true, message: 'このフィールドは必須です。' }]}
+          >
+            <Input placeholder="メール" />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[{ required: true, message: 'このフィールドは必須です。' }]}
+          >
+            <Input placeholder="パスワード" />
+          </Form.Item>
+         <Form.Item>
+         <input
+              type="file"
+              onChange={handleFileChange}
+              id="file"
+              style={{ display: 'none' }}
+            />
+         <label htmlFor="file" className="file-upload">
+            {previewUrl && (
+              <img className="formContainer-avt" src={previewUrl} alt="" />
+            )}
+            <div className="upload-btn">
+              <UploadOutlined style={{ fontSize: '20px', cursor: 'pointer' }} />{' '}
+              アバターをアップロード
             </div>
-        </div>
-    );
+          </label>
+         </Form.Item>
+          <Button
+            htmlType="submit"
+            size="large"
+            type="primary"
+            disabled={loading}
+          >
+            登録
+          </Button>
+          <p>
+            アカウントをお持ちですか？ <Link to="/login">ログイン</Link>
+          </p>
+        </Form>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
